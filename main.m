fdada02cf0e2dd2df68193a049b210316eae8665
Daniel Miller <Peaches491@gmail.com>
2015-04-18 11:13:10 -0400

    Added vertical lines to plot_all

--- main.m
+++ main.m
@@ -4,15 +4,18 @@ addpath 'data/mount_tire/'
 addpath 'data/remove_tire/'
 
 format long
-data = csvread('data/remove_tire/world-wheel_LF.csv', 1, 0);
-t = (data(:, 1) - data(1))./(1000000000);
+data = csvread('data/remove_tire/wheel_LF-hand_L.csv', 1, 0);
+ns2sec = @(ns_val) ((ns_val - data(1))./(1000000000.0));
+t = (data(:, 1) - data(1))./(1000000000); % Convert NS to seconds
 
 start_pct = 0.0;
+end_pct = 0.97;
 
 st_idx = int32(size(t,1)*start_pct) + 1
+end_idx = int32(size(t,1)*end_pct)
 %st_idx = 1
-t = t(st_idx:end);
-x = data(st_idx:end, 2:end);
+t = t(st_idx:end_idx);
+x = data(st_idx:end_idx, 2:end);
 
 dx = x(2:end, :) - x(1:end-1, :);
 dt = t(2:end) - t(1:end-1);
@@ -22,7 +25,8 @@ ddx = dx(2:end, :) - dx(1:end-1, :);
 a = bsxfun (@rdivide, ddx, dt(1:end-1));
 
 clf
-plot_all(t, {x, v, a})
-
+plot_all(t, {x, v, a}, ...
+    {[ns2sec(1425398470039567000), ...
+     ns2sec(1425398471824730000)], [ns2sec(1425398474387841000)]})
 
 %highlight(x(t_1, 1), x(t_2, 1))
--- plot_all.m
+++ plot_all.m
@@ -1,4 +1,4 @@
-function h = plot_all(t, plots)
+function h = plot_all(t, plots, verticals)
 
 size(plots, 2);
 
@@ -8,7 +8,7 @@ for iter = 0:size(plots, 2)-1
     size(y, 2);
     for plot_col = 1:size(y, 2)
         plot_no = iter*3 + plot_col;
-        subplot(3, 3, plot_no);
+        subplot(size(plots, 2), size(y, 2), plot_no);
         color = 'r';
         switch plot_col
             case 1
@@ -20,8 +20,17 @@ for iter = 0:size(plots, 2)-1
         end
         
         hold on
-        plot(t(1:size(y, 1)), smooth(t(1:size(y, 1)), y(:, plot_col)), 'k-')
-        plot(t(1:size(y, 1)), y(:, plot_col), color);
+        plot(t(1:size(y, 1)), smooth(t(1:size(y, 1)), y(:, plot_col)), color)
+        %plot(t(1:size(y, 1)), y(:, plot_col), color);
+        
+        for vert_set_idx = 1:numel(verticals)
+            vert_set = cell2mat(verticals(vert_set_idx));
+            for v = 1:numel(vert_set)
+                ax = gca;
+                ax.ColorOrderIndex = vert_set_idx;
+                plot([vert_set(v), vert_set(v)], ylim)
+            end
+        end
     end
 end
 
