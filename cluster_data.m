4385524acd68821b44310b2eeaf4829621dae339
Daniel Miller <Peaches491@gmail.com>
2015-04-26 00:30:03 -0400

    Looping through clustering modes

---
+++ cluster_data.m
@@ -0,0 +1,28 @@
+function [c, segTimes] = cluster_data(data, t, mode, varargin)
+
+switch mode
+    case 1
+        c = kmeans(data, varargin{1});
+    case 2
+        c = clusterdata(data, 'cutoff', varargin{1});
+    case 3
+        c = vbgm(data', varargin{1});
+    case 4
+        gm = fitgmdist(data, varargin{1});
+        c = cluster(gm, data);
+    case 5
+        [~,U,~] = fcm(data, varargin{1}); %5 = number of clusters
+        [~,c] = max(U);
+        c = transpose(c);
+end
+
+last = 1;
+segTimes = [];
+for i = 2:length(c)
+    if c(i) ~= c(last)
+        segTimes = [segTimes; t(i)];
+    end
+    last = i;
+end
+
+end
--- main.m
+++ main.m
@@ -18,6 +18,7 @@ s.data(1).pos.comp
 
 % Convert NS to seconds
 ns2sec = @(ns_val) max((ns_val - s.max_ns_start)./(1000000000.0), 0);
+smooth_cols = @(t, x, varargs) cell2mat(cellfun(@(x)(smooth(t(1:numel(x)), x, varargs(1), 'moving')), num2cell(x, 1), 'UniformOutput', false));
 
 % Resample data to fixed interval
 s = resample_data(s, ns2sec(s.max_ns_start), ns2sec(s.min_ns_end), 0.01);
@@ -27,12 +28,71 @@ s = resample_data(s, ns2sec(s.max_ns_start), ns2sec(s.min_ns_end), 0.01);
 dataset = s.data(1);
 datasets = [];
 for i = 1:numel(s.data)
+    %datasets = [datasets, s.data(i).pos.mag];
     datasets = [datasets, s.data(i).vel.mag];
 end
 for i = 1:numel(datasets)
     size(datasets(i).Data);
     %data = [data; datasets(i).Data'];
 end
+
+
+%%
+data = [];
+smoothed_datasets = [];
+for i = 1:numel(datasets)
+    ds = smooth_dataset(datasets(i), 'moving', 25);
+    data = [data, ds.Data];
+    smoothed_datasets = [smoothed_datasets, ds];
+end
+
+data = [data, ds.Time];
+size(data)
+data = data(~any(isnan(data),2),:);
+t = dataset.pos.mag.Time;
+
+max_k = 8;
+num_modes = 5;
+errs = zeros(num_modes, max_k-1);
+for mode = 1:num_modes
+    for iter = 2:max_k
+        if mode ~= 2
+            [c, segTimes] = cluster_data(data, t, mode, iter);
+        else 
+            [c, segTimes] = cluster_data(data, t, mode, (iter+max_k*mode)/(2*num_modes*max_k));
+        end
+
+        num_cuts = numel(segTimes);
+
+        totErr = 0;
+        for i = 1:length(segTimes)
+            totErr = totErr + min(abs(segTimes(i) - s.manual.EndTime));
+        end
+        avgErr = totErr/length(segTimes);
+        errs(mode, iter-1) = avgErr;
+        clf
+        bar(errs(:, :)')
+    end
+end
+clf
+bar(errs')
+
+%%
+rows = 3;
+for i = 1:numel(smoothed_datasets)
+    subplot(rows, round(size(smoothed_datasets, 2)/rows), i);
+    hold on;
+    scatter(ds.Time(1:numel(c)), smoothed_datasets(i).Data(1:numel(c)), 12, c)
+    
+    dots = repmat('-', 1, numel(s.manual.EndTime));
+    dots(1) = '.';
+    
+    %plot(ds.Time(1:numel(c)), smoothed_datasets(i).Data(1:numel(c)), '-')
+    plot_all(smoothed_datasets(i), {s.states.Time, s.manual.EndTime, segTimes }, dots, {}, false)
+    title(smoothed_datasets(i).Name)
+end
+% 
+
 
 
 %% Plot Data
@@ -53,85 +113,6 @@ dots(1) = '.';
 
 clf
 plot_all(plots, {s.manual.EndTime }, dots, {'Hand->Wheel Position', 'Hand->Wheel Velocity'});
-
-
-%%
-data = [];
-smoothed_datasets = [];
-for i = 1:numel(datasets)
-    ds = smooth_dataset(datasets(i), 'lowess');
-    data = [data, ds.Data];
-    smoothed_datasets = [smoothed_datasets, ds];
-end
-
-data = [data, ds.Time];
-size(data)
-data = data(~any(isnan(data),2),:);
-
-switch 5
-    case 1
-        c = kmeans(data, 4);
-    case 2
-        c = clusterdata(data, 'cutoff', 1.15);
-    case 3
-        c = vbgm(data', min(25, size(data, 1)));
-    case 4
-        gm = fitgmdist(data, 5);
-        c = cluster(gm, data);
-    case 5
-        [center,U,objFcn] = fcm(data,5); %5 = number of clusters
-        [m,c] = max(U);
-        c = transpose(c);
-end
-num_clusters = max(c)
-
-last = 1;
-segTimes = [];
-for i = 2:length(c)
-    if c(i) ~= c(last)
-        segTimes = [segTimes; t(i)];
-    end
-    last = i;
-end
-
-totErr = 0;
-for i = 1:length(segTimes)
-    totErr = totErr + min(abs(segTimes(i) - s.manual.EndTime));
-end
-avgErr = totErr/length(segTimes)
-
-
-
-
-clf;
-% combos = combnk(1:size(data, 2), 3);
-% combos = sort(combos);
-% rows = 3
-% 
-% for i = 1:size(combos, 1)
-%     subplot(rows, round(size(combos, 1)/rows), i)
-%     X = data(:, combos(i, :));
-%     scatter3(X(:,1),X(:,2),X(:,3),10,c)
-%     title(mat2str(combos(i, :)))
-% end
-% 
-% figure();
-rows = 2;
-for i = 1:numel(smoothed_datasets)
-    subplot(rows, round(size(smoothed_datasets, 2)/rows), i);
-    hold on;
-    scatter(ds.Time(1:numel(c)), smoothed_datasets(i).Data(1:numel(c)), 12, c)
-    
-    dots = repmat('-', 1, numel(s.manual.EndTime));
-    dots(1) = '.';
-    
-    %plot(ds.Time(1:numel(c)), smoothed_datasets(i).Data(1:numel(c)), '-')
-    plot_all(smoothed_datasets(i), {s.states.Time, s.manual.EndTime, segTimes }, dots, {}, false)
-    title(smoothed_datasets(i).Name)
-end
-% 
-
-
 
 
 
